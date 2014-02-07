18e74afdc90e789b99f2f0317c0dd76a0e7e4254
Jeroen van Schagen <jeroen.van.schagen@gmail.com>
2014-02-07 15:32:17 +0100

    Fixed to java 1.7

--- src/main/java/nl/mad/commons/Objects.java
+++ src/main/java/nl/mad/commons/Objects.java
@@ -3,6 +3,11 @@
  */
 package nl.mad.commons;
 
+import static nl.mad.commons.ConstructorFinder.findMostSupportedConstructor;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Utility class for handling objects.
  *
@@ -14,4 +19,48 @@ public final class Objects {
     private Objects() {
     }
     
+    public static <T> T instantiate(Class<T> beanClass, Object... arguments) {
+        Class<?>[] argumentTypes = getArgumentTypes(arguments);
+        Constructor<T> constructor = findMostSupportedConstructor(beanClass, argumentTypes);
+        return instantiate(constructor, arguments);
+    }
+
+    public static Class<?>[] getArgumentTypes(Object... arguments) {
+        Class<?>[] types = new Class<?>[arguments.length];
+        for (int index = 0; index < types.length; index++) {
+            types[index] = arguments[index].getClass();
+        }
+        return types;
+    }
+
+    public static <T> T instantiate(Constructor<T> constructor, Object... arguments) {
+        Object[] invocationArguments = putInCorrectOrder(constructor.getParameterTypes(), arguments);
+        try {
+            return constructor.newInstance(invocationArguments);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Could not instantiate constructor.", e);
+        }
+    }
+    
+    private static Object[] putInCorrectOrder(Class<?>[] types, Object[] arguments) {
+        Object[] result = new Object[types.length];
+        for (int index = 0; index < types.length; index++) {
+            result[index] = findFirstOfType(types[index], arguments);
+        }
+        return result;
+    }
+    
+    private static Object findFirstOfType(Class<?> requiredType, Object[] values) {
+        for (Object value : values) {
+            if (isOfType(value, requiredType)) {
+                return value;
+            }
+        }
+        return null;
+    }
+    
+    private static boolean isOfType(Object value, Class<?> requiredType) {
+        return value != null && requiredType.isAssignableFrom(value.getClass());
+    }
+
 }
