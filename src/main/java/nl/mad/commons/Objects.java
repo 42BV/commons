a8dc02c5b269b04d55fce63a63b28f5fe862b7b2
Jeroen van Schagen <jeroen.van.schagen@gmail.com>
2014-02-07 15:05:12 +0100

    Initial commit

---
+++ src/main/java/nl/mad/commons/Objects.java
@@ -0,0 +1,17 @@
+/*
+ * (C) 2013 42 bv (www.42.nl). All rights reserved.
+ */
+package nl.mad.commons;
+
+/**
+ * Utility class for handling objects.
+ *
+ * @author Jeroen van Schagen
+ * @since Feb 7, 2014
+ */
+public final class Objects {
+    
+    private Objects() {
+    }
+    
+}
